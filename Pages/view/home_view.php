0cc0faf1512bf6fb3eae92b8e1d4b10d5e2a8a67
kavish <101743@glr.nl>
2026-01-09 14:06:03 +0100

    mappenstructuur en begin home-pagina

---
+++ Pages/view/home_view.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href=".">
+</head>
+<body>
+<header>
+    <nav class="nav">
+        <div class="nav-left">
+            <a href="#">Home</a>
+            <a href="#">Inschrijven</a>
+            <a href="#">Modellen zoeken</a>
+        </div>
+
+        <div class="nav-right">
+            <a href="#">Profiel</a>
+        </div>
+    </nav>
+</header>
+</body>
+</html>

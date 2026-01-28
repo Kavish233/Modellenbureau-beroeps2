645da3de4a5274be21406b1a534b9899b3036391
102780 <102780@glr.nl>
2026-01-28 13:53:29 +0100

    Telefoon view

--- Pages/view/modellen-overzicht_view.php
+++ Pages/view/modellen-overzicht_view.php
@@ -9,7 +9,7 @@ $uploadsPath = "/beroeps/Modellenbureau/Pages/uploads/";
     <title>Modellen zoeken</title>
     <link rel="stylesheet" href="/beroeps/Modellenbureau/CSS/main.css">
     <link rel="stylesheet" href="/beroeps/Modellenbureau/CSS/modellen-overzicht.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <script src="/beroeps/Modellenbureau/Script/menu.js" defer></script>
 </head>
 <body>
